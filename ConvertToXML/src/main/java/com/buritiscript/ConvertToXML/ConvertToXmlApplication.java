7138059a249a7b8d1ba8aa2d1417be5edfddedbf
Emerson Reis da Costa <emerson.costa@pmbv.local>
2023-05-19 17:31:29 -0400

    projeto criado

---
+++ ConvertToXML/src/main/java/com/buritiscript/ConvertToXML/ConvertToXmlApplication.java
@@ -0,0 +1,13 @@
+package com.buritiscript.ConvertToXML;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConvertToXmlApplication {
+	
+	public static void main(String[] args) {
+		SpringApplication.run(ConvertToXmlApplication.class, args);
+	}
+
+}
---
+++ ConvertToXML/src/test/java/com/buritiscript/ConvertToXML/ConvertToXmlApplicationTests.java
@@ -0,0 +1,13 @@
+package com.buritiscript.ConvertToXML;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConvertToXmlApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
